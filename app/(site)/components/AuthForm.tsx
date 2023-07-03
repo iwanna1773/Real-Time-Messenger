85d6d8e3ae07ef9c1b04dcd4676a5e66e1073dd6
Tam Nguyen <thanhtam.nguyen@planet1world.com>
2023-07-04 04:21:09 +0700

    init component Button

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,11 +4,12 @@ import React, { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 
 import Input from "@/app/components/inputs/Input";
+import Button from "@/app/components/Button";
 
 type Variant = "LOGIN" | "REGISTER";
 
 function AuthForm() {
-  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [variant, setVariant] = useState<Variant>("REGISTER");
   const [isLoading, setIsLoading] = useState(false);
 
   const defaultValues = {
@@ -48,11 +49,49 @@ function AuthForm() {
     setIsLoading(true);
   };
 
+  const showFormRegister = () => {
+    if (variant === "REGISTER") {
+      return (
+        <>
+          <Input id="name" label="Name" errors={errors} register={register} />
+
+          <Input id="email" label="Email" errors={errors} register={register} />
+          <Input
+            id="password"
+            label="Password"
+            errors={errors}
+            register={register}
+          />
+          <Button disabled={isLoading} fullWidth type="submit">
+            Register
+          </Button>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gay-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="bg-white px-2 text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="mt-6 flex gap-2">
+              <AuthSocialButton />
+            </div>
+          </div>
+        </>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input />
+          {showFormRegister()}
         </form>
       </div>
     </div>
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -16,7 +16,44 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = (props) => {
   const { id, label, type, required, register, errors, disabled } = props;
-  return <div>Input</div>;
+  return (
+    <div>
+      <label
+        className="block text-sm font-medium leading-6 text-gray-900"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          className={clsx(
+            `
+          form-input block 
+          w-full 
+          roundend-md 
+          border-0 
+          py-1.5
+          text-gray-900 
+          shadow-sm ring-1 
+          ring-inset 
+          ring-gray-300 
+          placeholder:text-gray-400 
+          focus:ring-2 
+          focus:ring-inset 
+          focus:ring-sky-600 
+          sm:text-sm sm:leading-6`,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+          {...register(id, { required })}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
